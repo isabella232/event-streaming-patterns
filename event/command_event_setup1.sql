fcd6ce088c69e64c36147f1fb653b4a20b8a2922
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-06-15 14:32:03 -0600

    Deployed 5cbb706 with MkDocs version: 1.1.2

--- event/command_event_setup1.sql
+++
@@ -1,8 +0,0 @@
-CREATE STREAM dispatch_products (
-  order_id BIGINT KEY,
-  address VARCHAR
-) WITH (
-  KAFKA_TOPIC = ' dispatch_products',
-  VALUE_FORMAT = 'AVRO',
-  PARTITIONS = 2
-);
--- stream-processing/wait-for-n-events-setup2.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE trade_approval AS
-  SELECT trade_id, count(*) AS approvals
-  FROM trade_review
-  WHERE approved = TRUE
-  GROUP BY trade_id;
